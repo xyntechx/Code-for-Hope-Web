ec21259328a2057f3bba3d2ffb985a11b0172cbd
xyntechx <xyntechx@gmail.com>
2021-12-26 09:52:44 +0800

    🎉 v1

---
+++ README.md
@@ -0,0 +1,14 @@
+# Code for Hope IDE
+An online IDE for Code for Hope students and mentors.
+
+## 🤖 Technologies
+- Svelte
+- TypeScript
+
+## 🔨 Usage
+```bash
+npm install
+```
+```bash
+npm run dev
+```
---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<main>
+    <h1>Code for Hope IDE</h1>
+    <iframe
+        title="IDE"
+        frameborder="0"
+        src="https://replit.com/@xyntechx/CodeForHope?embed=true"
+    />
+</main>
+
+<style>
+    main {
+        text-align: center;
+        width: 100vw;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+
+    h1 {
+        color: #818cf8;
+        font-size: 2.5rem;
+    }
+
+    iframe {
+        width: 90vw;
+        height: 85vh;
+    }
+
+    @media (max-width: 640px) {
+        h1 {
+            font-size: 1.5rem;
+        }
+    }
+</style>
