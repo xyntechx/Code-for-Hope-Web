9aeea6fa2735b1a8bd154134d5ad7af4234f333a
xyntechx <xyntechx@gmail.com>
2021-12-26 13:47:12 +0800

    ✨ Notebook links

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,39 @@
 <main>
-    <h1>Code for Hope IDE</h1>
+    <h1>Code for Hope</h1>
+    <section>
+        <a
+            href="https://colab.research.google.com/drive/19ceCfHiU4sCW2Sdp5PF4JQ6mautEeFoJ"
+            target="_blank">Lesson 1</a
+        >
+        <a
+            href="https://colab.research.google.com/drive/1PCfgcUbWtm6MqjyN1s7sLn13ncHag1JY"
+            target="_blank">Practice 1</a
+        >
+        <a
+            href="https://colab.research.google.com/drive/1NqPWP02JuXNdUyK0w0AWBCg9oZVSK1tf"
+            target="_blank">Lesson 2</a
+        >
+        <a
+            href="https://colab.research.google.com/drive/1P7EZdJLo26vlZEJ2q17JcF8Z40qUPRMx"
+            target="_blank">Practice 2</a
+        >
+        <a
+            href="https://colab.research.google.com/drive/1xMIlF43QZVT_dAlAVbBZ6T2r4t3DTM61"
+            target="_blank">Lesson 3</a
+        >
+        <a
+            href="https://colab.research.google.com/drive/1oUaTxuVtr6qZT7cH_mubSsfEuDZ5d9pw"
+            target="_blank">Practice 3</a
+        >
+        <a
+            href="https://colab.research.google.com/drive/11TCo3jqDKbHyis-q5Tn17l4toqKUPUgw"
+            target="_blank">Lesson 4</a
+        >
+        <a
+            href="https://colab.research.google.com/drive/1opPVymG1lmYuFFJZ7pz6D5GCVYe5r3wd"
+            target="_blank">Practice 4</a
+        >
+    </section>
     <iframe
         title="IDE"
         frameborder="0"
@@ -24,12 +58,31 @@
 
     iframe {
         width: 90vw;
-        height: 85vh;
+        height: 80vh;
+    }
+
+    section {
+        display: grid;
+        column-gap: 1rem;
+        grid-template-columns: repeat(8, minmax(0, 1fr));
+        margin-bottom: 1rem;
+    }
+
+    a {
+        font-size: 1rem;
     }
 
     @media (max-width: 640px) {
         h1 {
             font-size: 1.5rem;
+        }
+
+        section {
+            column-gap: 0.2rem;
+        }
+
+        a {
+            font-size: 0.5rem;
         }
     }
 </style>
