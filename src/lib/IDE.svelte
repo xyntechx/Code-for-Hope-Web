cb5b02ce74f97e242f8934fb69b5083ffb6e8c79
xyntechx <xyntechx@gmail.com>
2021-12-28 09:42:53 +0800

    ✨ Separate IDE page

---
+++ src/lib/IDE.svelte
@@ -0,0 +1,12 @@
+<iframe
+    title="IDE"
+    frameborder="0"
+    src="https://replit.com/@xyntechx/CodeForHope?embed=true"
+/>
+
+<style>
+    iframe {
+        width: 90vw;
+        height: 85vh;
+    }
+</style>
