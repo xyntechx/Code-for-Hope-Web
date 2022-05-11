53e8a588e0c117537d05bc52312fc5adc81c7f54
xyntechx <xyntechx@gmail.com>
2022-05-12 07:40:38 +0800

    ✨ PyScript py-repl feature

--- src/lib/IDE.svelte
+++ src/lib/IDE.svelte
@@ -1,12 +1,19 @@
-<iframe
-    title="IDE"
-    frameborder="0"
-    src="https://replit.com/@xyntechx/CodeForHope?embed=true"
-/>
+<head>
+    <link rel="stylesheet" href="https://pyscript.net/alpha/pyscript.css" />
+    <script defer src="https://pyscript.net/alpha/pyscript.js"></script>
+</head>
+
+<py-repl />
 
 <style>
-    iframe {
+    py-repl {
         width: 90vw;
         height: 85vh;
+        text-align: left;
+        background-color: #818cf8;
+        overflow: scroll;
+        padding: 1rem;
+        border-radius: 0.5rem;
+        border: #fff 1px solid;
     }
 </style>
